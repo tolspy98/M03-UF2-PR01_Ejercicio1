5c8d743a1ad9ff63ac8471f4f680e616cb1dba90
tolspy98 <rubensanchezg2001@gmail.com>
2024-12-11 20:59:22 +0100

    Creado el primer perfil

--- src/Main.java
+++ src/Main.java
@@ -6,6 +6,15 @@ public class Main {
 
     public void inicio() {
 
+        Perfil RubenSanchez = new Perfil(
+                "Rubén Sánchez",
+                "Desde pequeño que me gustan los ordenadores y ahí sigo",
+                new String[] {"💻Programar (Aunque se me dé regular)", "🎮Jugar (Aunque se me dé regular)", "🍳Cocinar (Aunque se me dé regular)"},
+                new String[] {"🍕Pizza", "🍔Hamburguesa", "🍨Helado"},
+                new String[] {"Me encantan los gatos🐈 (Aunque soy alérgico)", "Me encantan los perros🐕 (Aunque soy alérgico)", "🔥Siempre se me queman los huevos, pero uno se acostumbra al sabor"}
+        );
+
+        System.out.println(RubenSanchez.getProfileInfo());
 
     }
 
